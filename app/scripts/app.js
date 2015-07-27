bbe47c4779afe61c72d95f56d2590c0911c28525
Valentin Polo <me@valentinpolo.fr>
2015-07-27 18:05:40 +0200

    Angular installation

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cvsFrontApp
+ * @description
+ * # cvsFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cvsFrontApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvsFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cvsFrontApp
+ */
+angular.module('cvsFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
