e18461d1244a1d056d4f43a0319f55fab6b88b39
Valentin Polo <me@valentinpolo.fr>
2015-07-30 15:00:19 +0200

    Wrong merging

---
+++ app/scripts/controllers/registerRecruiter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cvsApp').controller('RegisterRecruiterCtrl', ['$scope', function($scope) {
+
+  $scope.init = function() {
+    console.log('Triggered from RegisterRecruiterCtrl!');
+  };
+
+  $scope.init();
+
+}]);
