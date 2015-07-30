94e5ec523834ab059a7864b6cd7caf2e0e934038
Valentin Polo <me@valentinpolo.fr>
2015-07-30 19:44:20 +0200

    Progressing on form!

--- app/scripts/controllers/registerRecruiter.js
+++ app/scripts/controllers/registerRecruiter.js
@@ -2,10 +2,15 @@
 
 angular.module('cvsApp').controller('RegisterRecruiterCtrl', ['$scope', function($scope) {
 
-  $scope.init = function() {
-    console.log('Triggered from RegisterRecruiterCtrl!');
+  $scope.newRecruiter = {
+    user: {},
+    recruiter: {
+      availability: 'all'
+    }
   };
 
-  $scope.init();
+  $scope.register = function() {
+    console.log('we need to register this new recruiter!');
+  };
 
 }]);
