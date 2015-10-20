0a8f27cfcbd430deda03d6b24837197e8383a684
Valentin Polo <me@valentinpolo.fr>
2015-10-20 07:15:43 +0200

    Almost done on SMS sending

--- app/scripts/controllers/admin/messagingNewSMS.js
+++ app/scripts/controllers/admin/messagingNewSMS.js
@@ -14,20 +14,24 @@ angular.module('cvsApp').controller('AdminMessagingNewSMSCtrl',
           groups: []
         };
         $scope.message = '';
+        $scope.remainingSMSCredits = {};
       }
       initForm();
 
-      function estimateSendingCost() {
-        var pricePerSMS = 0.024;
+      function estimateNumberSent() {
         if ($scope.sendingType === 'toUsers') {
-          return $scope.selected.users.length.toFixed(3) * pricePerSMS;
+          return $scope.selected.users.length.toFixed(4);
         } else if ($scope.sendingType === 'toGroups') {
           var totalUsers = 0;
           for (var i = 0; i < $scope.selected.groups.length; i++) {
             totalUsers += $scope.selected.groups[i].length;
           }
-          return totalUsers.toFixed(3) * pricePerSMS;
+          return totalUsers.toFixed(4);
         }
+      }
+
+      function estimateSendingCost() {
+        return estimateNumberSent() * 0.0528;
       }
 
       // Fetching phone numbers
@@ -40,43 +44,57 @@ angular.module('cvsApp').controller('AdminMessagingNewSMSCtrl',
         $scope.groups = usersGroups.plain();
       });
 
+      Restangular.one('messaging/remaining-sms-credits').get().then(function(remainingSMSCredits) {
+        $scope.remainingSMSCredits = remainingSMSCredits.plain();
+      });
+
       $scope.canBeSent = function() {
         return $scope.message !== '' &&
-            ( $scope.selected.users.length || $scope.selected.groups.length );
+          ( $scope.selected.users.length || $scope.selected.groups.length );
       };
 
       // Sending SMS
       $scope.sendSMS = function() {
-        SweetAlert.swal({
-            html: true,
-            title: "Êtes-vous certain ?",
-            text: "Votre message est sur le point d'être envoyé.<br>" +
-            "Souhaitez-vous poursuivre ?<br><br>" +
-            "<strong>Le coût estimé de cet envoi est de " + $filter('number')(estimateSendingCost(), 2).toLocaleString() + " &euro;</strong>",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#00C853", confirmButtonText: "Oui, envoyer !",
-            cancelButtonText: "Non, annuler",
-            closeOnConfirm: false,
-            closeOnCancel: false },
-          function(isConfirm){
-            if (isConfirm) {
-              Restangular.all('messaging/send-sms')
-                .customPOST({
-                  sendingType: $scope.sendingType,
-                  selection: $scope.selected,
-                  message: $scope.message
-                })
-                .then(function(success) {
-                  SweetAlert.swal("Message envoyé !", "Votre message a bien été envoyé.", "success");
-                  initForm();
-                }, function(error) {
-                  SweetAlert.swal({html: true, title:"Échec de l'envoi", text: "Une erreur est survenue pendant l'envoi du message.<br>Merci de réessayer dans quelques instants...", type: "error"});
-                });
-            } else {
-              SweetAlert.swal("Envoi annulé", "Soyez sans crainte, l'envoi a bien été annulé.", "error");
+        if ($scope.remainingSMSCredits.remaining_sms > estimateNumberSent()) {
+          SweetAlert.swal({
+              html: true,
+              title: "Êtes-vous certain ?",
+              text: "Votre message est sur le point d'être envoyé.<br>" +
+              "Souhaitez-vous poursuivre ?<br><br>" +
+              "<strong>Cet envoi consommera " + $filter('number')(estimateNumberSent(), 0) + " crédits sur vos " + $scope.remainingSMSCredits.remaining_sms + " restants.</strong><br>" +
+              "Le coût estimé de cet envoi est de " + $filter('number')(estimateSendingCost(), 3).toLocaleString() + " &euro;",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#00C853", confirmButtonText: "Oui, envoyer !",
+              cancelButtonText: "Non, annuler",
+              closeOnConfirm: false,
+              closeOnCancel: false },
+            function (isConfirm){
+              if (isConfirm) {
+                Restangular.all('messaging/send-sms')
+                  .customPOST({
+                    sendingType: $scope.sendingType,
+                    selection: $scope.selected,
+                    message: $scope.message
+                  })
+                  .then(function(success) {
+                    SweetAlert.swal("Message envoyé !", "Votre message a bien été envoyé.", "success");
+                    initForm();
+                  }, function(error) {
+                    SweetAlert.swal({html: true, title:"Échec de l'envoi", text: "Une erreur est survenue pendant l'envoi du message.<br>Merci de réessayer dans quelques instants...", type: "error"});
+                  });
+              } else {
+                SweetAlert.swal("Envoi annulé", "Soyez sans crainte, l'envoi a bien été annulé.", "error");
+              }
             }
-          });
+          );
+        } else {
+          SweetAlert.swal({
+            html: true,
+            title:"Envoi impossible !",
+            text: "Cet envoi requiert " + $filter('number')(estimateNumberSent(), 0) + " crédit(s)<br>" +
+            "et votre compte n'en dispose que de " + $scope.remainingSMSCredits.remaining_sms + ".", type: "error"});
+        }
       };
 
     }
