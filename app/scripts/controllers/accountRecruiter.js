97942a0deccd80d5422a769aa5ab974c16663f8d
Nicolas <nducom@gmail.com>
2015-10-23 00:05:22 +0200

    fixed popup bug on interview mouseover

--- app/scripts/controllers/accountRecruiter.js
+++ app/scripts/controllers/accountRecruiter.js
@@ -5,6 +5,7 @@ angular.module('cvsApp').controller('AccountRecruiterCtrl',
     function($scope) {
 
       $scope.user = $scope.$parent.user;
+      $scope.accountPopover = {templateUrl: 'popover.html'};
 
       $scope.setSelectedAccountInterview = function(interview) {
         $scope.selectedAccountInterview = interview;
