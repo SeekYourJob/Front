8e123604bbe323fdf87f9e0734cf70f5caa9f784
Nicolas <nducom@gmail.com>
2015-10-31 18:44:25 +0100

    progress on file upload

--- app/scripts/controllers/accountRecruiter.js
+++ app/scripts/controllers/accountRecruiter.js
@@ -1,14 +1,51 @@
 'use strict';
 
 angular.module('cvsApp').controller('AccountRecruiterCtrl',
-  ['$scope',
-    function($scope) {
+  ['$scope','Upload','ENV',
+    function($scope,Upload,ENV) {
 
       $scope.user = $scope.$parent.user;
       $scope.accountPopover = {templateUrl: 'popover.html'};
 
+      $scope.form = {
+        others: '',
+        emailRecruiterToAdd: '',
+        dataRecruiterToAdd: {},
+        documents: false,
+        emailAlreadyExists: false,
+        documentIsBeingSent: false,
+        isBeingSubmitted: false,
+        isSubmitted: false
+      };
+
       $scope.setSelectedAccountInterview = function(interview) {
         $scope.selectedAccountInterview = interview;
+      };
+
+      $scope.$watch('form.documents', function() {
+        $scope.uploadDocuments($scope.form.documents);
+      });
+
+      $scope.uploadDocuments = function(files) {
+        /*jshint -W083 */
+        if (files && files.length) {
+          for (var i = 0; i < files.length; i++) {
+            $scope.form.documentIsBeingSent = true;
+            var file = files[i];
+            Upload.upload({
+              url: ENV.apiEndpoint + '/documents',
+              data:{file: file,
+                  'user': $scope.user.ido},
+              sendFieldsAs: 'form',
+              skipAuthorization: true
+            }).success(function(document) {
+              $scope.form.documentIsBeingSent = false;
+            }).error(function(data, status, headers, config) {
+              console.log('ERROR', data, status, headers, config);
+              $scope.form.documentIsBeingSent = false;
+            });
+          }
+        }
       };
 
     }
