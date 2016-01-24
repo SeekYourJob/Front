812ff31a630692abfc5395a17af68b04cb660748
Nicolas <nducom@gmail.com>
2016-01-24 18:58:09 +0100

    adding candidate registration page

--- app/scripts/controllers/registerCandidate.js
+++ app/scripts/controllers/registerCandidate.js
@@ -1,10 +1,20 @@
 'use strict';
 
-angular.module('cvsApp').controller('RegisterCandidateCtrl', ['$scope', function($scope) {
+angular.module('cvsApp').controller('RegisterCandidateCtrl',  ['$scope', '$http', 'Upload', 'AuthService', '$state', 'ENV', 'UploadService',
+  function($scope, $http, Upload, AuthService, $state, ENV, UploadService) {
 
-  $scope.init = function() {
-    console.log('Triggered from a controller!');
-  };
+    $scope.grades = ['L3','M1','M2'];
+
+    $scope.newCandidate = {
+      user: {},
+      candidate: {
+        availability: '',
+        grade: '',
+        documents: []
+      }
+    };
+
+
 
   $scope.init();
 
