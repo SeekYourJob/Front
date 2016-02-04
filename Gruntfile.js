34c17c088bf976908537d474ae7d5d45b15e4e4f
Valentin Polo <me@valentinpolo.fr>
2016-02-04 23:21:52 +0100

    Testing consolelog remove

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,6 +320,14 @@ module.exports = function (grunt) {
       }
     },
 
+    uglify: {
+      options: {
+        compress: {
+          drop_console: true
+        }
+      }
+    },
+
     // The following *-min tasks will produce minified files in the dist folder
     // By default, your `index.html`'s <!-- Usemin block --> will take care of
     // minification. These next options are pre-configured if you do not wish
