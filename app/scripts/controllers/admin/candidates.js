4e6c6b5347e52cd67cd796ac09b338683b31ff3d
Valentin Polo <me@valentinpolo.fr>
2016-02-05 23:34:56 +0100

    auto redirect on search

--- app/scripts/controllers/admin/candidates.js
+++ app/scripts/controllers/admin/candidates.js
@@ -3,12 +3,19 @@
  */
 'use strict';
 
-angular.module('cvsApp').controller('AdminCandidatesCtrl', ['$scope', 'Restangular',
+angular.module('cvsApp').controller('AdminCandidatesCtrl', ['$scope', 'Restangular', '$state',
+    function($scope, Restangular, $state) {
 
-    function($scope, Restangular) {
         Restangular.all('candidates').getList().then(function(candidates) {
             $scope.candidatesCollection = candidates;
             $scope.candidatesDisplayedCollection = [].concat($scope.candidatesCollection);
         });
+
+        $scope.$watch('candidatesDisplayedCollection', function(candidates) {
+          if (Array.isArray(candidates) && candidates.length === 1) {
+            $state.go('app.admin.candidates.details', {id: candidates[0].ido});
+          }
+        });
+
     }
 ]);
--- app/scripts/controllers/admin/recruiters.js
+++ app/scripts/controllers/admin/recruiters.js
@@ -1,12 +1,18 @@
 'use strict';
 
-angular.module('cvsApp').controller('AdminRecruitersCtrl', ['$scope', 'Restangular',
+angular.module('cvsApp').controller('AdminRecruitersCtrl', ['$scope', 'Restangular', '$state',
+  function($scope, Restangular, $state) {
 
-  function($scope, Restangular) {
     Restangular.all('recruiters').getList().then(function(recruiters) {
       $scope.recruitersCollection = recruiters;
       $scope.recruitersDisplayedCollection = [].concat($scope.recruitersCollection);
     });
-  }
 
+    $scope.$watch('recruitersDisplayedCollection', function(recruiters) {
+      if (Array.isArray(recruiters) && recruiters.length === 1) {
+        $state.go('app.admin.recruiters.details', {id: recruiters[0].ido});
+      }
+    });
+
+  }
 ]);
